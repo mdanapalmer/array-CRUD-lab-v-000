f0a9b7c9c5362714d2e162869716dfb3075c9ef7
Dana Palmer <roiai4@hotmail.com>
2018-10-07 02:04:33 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,6 +6,6 @@ def create_an_array
   cats_array = ["Waffles", "Newton", "Zoey", "Godiva"]
 end
 
-def add_element_to_end_of_array
-  cats_array << "Cricket"
+def add_element_to_end_of_array(array, element)
+  array << element
 end
